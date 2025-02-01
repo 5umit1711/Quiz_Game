801df19e391b3ae1f3c4abada2632c5f148570a7
Sumit <sumit1711kd@gmail.com>
2025-02-01 18:58:26 +0530

    Change the config again

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.jsonserve.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
